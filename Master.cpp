9272baeb57f762a58541a939015bfcffd81daca8
xtq <belldandyxtq@gmail.com>
2014-08-15 00:10:46 +0900

    Master

--- Master.cpp
+++ Master.cpp
@@ -1,10 +1,67 @@
-/*
+/*OB
  * Master.cpp
  *
  *  Created on: Aug 8, 2014
  *      Author: xtq
  */
 
-#include
+#include "Master.h"
+#include <unistd.h>
 
+Master::node_info::node_info(std::string ip, std::size_t total_memory,  unsigned int node_id):
+	ip(ip), 
+	blocks(block_info()), 
+	avaliable_memory(total_memory), 
+	total_memory(total_memory), 
+	node_id(node_id)
+{}
 
+Master::Master():
+	IOnodes(std::vector<node_info>()), 
+	number_nodes(0), 
+	files(file_info()), 
+	_id_pool(new bool[MAX_NODE_NUMBER]), 
+	_now_node_number(0)
+{
+	memset(_id_pool, 0, MAX_NODE_NUMBER*sizeof(bool)); 
+}
+
+int Master::_add_IO_node(std::string node_ip, std::size_t total_memory)throw(std::bad_alloc)
+{
+	int id=0; 
+	try
+	{
+		id=_get_node_id(); 
+	}
+	catch(std::bad_alloc &e)
+	{
+		throw; 
+	}
+	IOnodes.push_back(node_info(node_ip, total_memory, node_id));
+	return 1; 
+}
+
+int Master::_get_node_id()throw(std::bad_alloc)
+{
+	for(; _now_node_number<MAX_NODE_NUMBER; ++_now_node_number)
+	{
+		if(!_id_pool[_now_node_number])
+		{
+			_id_pool[_now_node_number]=true; 
+			return _now_node_number; 
+		}
+	}
+	for(_now_node_number=0;  _now_node_number<MAX_NODE_NUMBER; ++_now_node_number)
+	{
+		if(!_id_pool[_now_node_number])
+		{
+			_id_pool[_now_node_number]=true; 
+			return _now_node_number;
+		}
+	}
+	throw std::bad_alloc(); 
+}
+
+void Master::_init_server() throw(std::runtime_error)
+{
+	
--- Master.h
+++ Master.h
@@ -10,7 +10,8 @@
 
 #include <map>
 #include <vector>
-#include <unistd.h>
+#include <string>
+
 
 class Master
 {
@@ -22,31 +23,36 @@ public:
 private:
 	//map file_no, vector<start_point>
 	typedef std::map<int, std::vector<std::size_t> > block_info; 
+	typedef std::map<std::string,  int> file_info; 
+
 	class node_info
 	{
 	public:
-		node_info(std::string ip, std::size_t avaliable_memory); 
+		node_info(std::string ip, std::size_t total_memory,  unsigned int node_id); 
 		~node_info();
 		
 		std::string ip; 
-		block_info blocks; 
+		block_info blocks;
+		unsigned int node_id; 
 		std::size_t avaliable_memory; 
 		std::size_t total_memory; 
 	}
 
 private:
-	class node_info
-	{
-	public:
-		node_info();
-		
-		std::
+	std::vector<node_info> IOnodes;
+	unsigned int number_node;
+	file_info files; 
+	bool *_id_pool; 
+	unsigned int _now_node_number; 
+	struct sockaddr_in _server_addr; 
+	int _server_socket; 
 
 private:
-	std::vector<> IOnodes;
-	static int number_node;
-	std::queue<class block> buffer_queue;
-	
+	int _add_IO_node(std::string node_ip, std::size_t avaliable_memory);
+	int _add_file(std::string file_path);
+	int _get_file_blocks(std::string file_path);  
+	int _get_node_id()throw(std::bad_alloc); 
+	void _init_server()throw(std::runtime_error); 
 };
 
 #endif
