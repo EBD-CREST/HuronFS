6a01813d1633f5a244e251c95846f1bf4b76dcfd
xtq <Belldandyxtq@gmail.com>
2017-02-10 22:02:03 +0900

    add LGPL license

--- tests/test1/prepare.sh
+++ tests/test1/prepare.sh
@@ -1,3 +1,25 @@
+# Copyright (c) 2017, Tokyo Institute of Technology
+# Written by Tianqi Xu, xu.t.aa@m.titech.ac.jp.
+# All rights reserved. 
+# 
+# This file is part of HuronFS.
+# 
+# Please also read the file "LICENSE" included in this package for 
+# Our Notice and GNU Lesser General Public License.
+# 
+# This program is free software; you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License (as published by the Free Software 
+# Foundation) version 2.1 dated February 1999. 
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU 
+# General Public License for more details. 
+# 
+# You should have received a copy of the GNU Lesser General Public License along 
+# with this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
+# Place, Suite 330, Boston, MA 02111-1307 USA
+
 #!/bin/bash
 
 echo "this is test file" > /tmp/test
